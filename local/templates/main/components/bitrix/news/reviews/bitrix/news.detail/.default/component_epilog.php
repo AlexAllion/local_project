c91785568413afaca0d4aa971dbd7b7734e279f5
AlexAllion <olhovik.ay@gmail.com>
2024-11-04 21:47:33 +0700

    main

--- local/templates/main/components/bitrix/news/reviews/bitrix/news.detail/.default/component_epilog.php
+++
@@ -1,4 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();global $APPLICATION;
-$APPLICATION->SetPageProperty ('title',$arResult["PROPERTIES"]['JOB_TITLE']['VALUE'].' '.$arResult["PROPERTIES"]['COMPANY']['VALUE']);
-
-/*$APPLICATION->SetTitle($arResult['NAMwwE']);*/
